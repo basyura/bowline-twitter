8f7591b0c6bc190157b451c3ae19e259283b8d50
basyura <basyura@gmail.com>
2010-08-01 20:47:14 +0900

    to application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,29 @@
 jQuery(function($){
+  Bowline.trace = true;
+
   $("#tweets").update(function(){
-    $(this).attr({
-      scrollTop: $(this).attr("scrollHeight")
+      $(this).attr({
+          scrollTop: $(this).attr("scrollHeight")
+        });
     });
-  });
+
+  var tweets = $('#tweets');
+  tweets.bowlineBind('TweetsBinder');
+
+  $('#updateText').keydown(function(e){
+      if(e.keyCode != 13) {
+        return;
+      }
+      tweets.invoke('update', $('#updateText').val());
+      $('#updateText').val('');
+      return false;
+    });
+
+  $('#friends').click(function() {
+      tweets.invoke('friends');
+    });
+
+  $('#mentions').click(function() {
+      tweets.invoke('mentions');
+    });
 });
