b208d3f18d16302e5cb622bc49225f060d3153cb
basyura <basyura@gmail.com>
2010-08-07 17:37:13 +0900

    css dock menu http://www.ndesign-studio.com/blog/css-dock-menu

--- app/windows/main_window.rb
+++ app/windows/main_window.rb
@@ -1,7 +1,7 @@
 class MainWindow < Bowline::Desktop::WindowManager
   setup!
   self.file   = :index
-  set_size(350, 710)
+  set_size(300, 710)
   center
   enable_developer
   on_load { show }
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,15 +22,65 @@ jQuery(function($){
       return false;
     });
 
-  $('#btn_friends').click(function() {
-      //tweets.invoke('friends');
+  $('#dock_home').click(function() {
       tweets.show();
       mentions.hide();
     });
 
-  $('#btn_mentions').click(function() {
-      //tweets.invoke('mentions');
+  $('#dock_mentions').click(function() {
       mentions.show();
       tweets.hide();
     });
+
+  $('#dock_post').click(function() {
+      var text = $('#post_text');
+      if(text.size() == 0) {
+        text = $(document.createElement('textarea'));
+        text.attr("id" , "post_text");
+        text.css({
+            "position":"absolute",
+            "top"     :"0",
+            "width"   :"97%",
+            "height"  :"40px",
+          });
+        // post event
+        text.keydown(function(e) {
+            // enter
+            if(e.keyCode == 13 && e.ctrlKey) {
+              $(this).attr("disabled" , "disabled");
+              setTimeout(function() {
+                  var text = $("#post_text");
+                  tweets.invoke('update', text.val());
+                  text.val('');
+                  text.hide();
+                },100);
+            }
+            // esc
+            else if(e.keyCode == 27) {
+              $(this).hide();
+            }
+          });
+        // hide on blur
+        text.blur(function() {
+            $(this).hide();
+          });
+        $('body').append(text);
+      }
+      text.attr("disabled" , "");
+      text.show();
+      text.focus();
+    });
+
+
+  $('#dock').Fisheye(
+    {
+      maxWidth: 50,
+      items: 'a',
+      itemsText: 'span',
+      container: '.dock-container',
+      itemWidth: 40,
+      proximity: 90,
+      halign : 'center'
+    }
+  )
 });
